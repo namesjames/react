1af44918ae1cf0ec5f78dc42105039f1fdd3fed0
J ª/\/\ ∑ $ <james@galvanize.com>
2016-08-01 00:00:03 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+var myArticle = {
+ article: <article></article>,
+};
+
+var Pistons2004 = {
+  center:        <li>Ben Wallace</li>,
+  powerForward:  <li>Rasheed Wallace</li>,
+  smallForward:  <li>Tayshaun Prince</li>,
+  shootingGuard: <li>Richard Hamilton</li>,
+  pointGuard:    <li>Chauncey Billups</li>
+};

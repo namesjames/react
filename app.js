abdd9b7bfe754e24c95f2eda9481e1fc560098e6
J ª/\/\ ∑ $ <james@galvanize.com>
2016-08-06 23:49:27 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var myArticle = {
- article: <article></article>,
-};
+var h1 = <h1>Hello world</h1>;
+
+var myArticle = <article></article>;
 
 var Pistons2004 = {
   center:        <li>Ben Wallace</li>,
@@ -9,3 +9,15 @@ var Pistons2004 = {
   shootingGuard: <li>Richard Hamilton</li>,
   pointGuard:    <li>Chauncey Billups</li>
 };
+
+var p1 = <p id="large">foo</p>;
+
+var p2 = <p id="small">bar</p>;
+
+ var theGoogle = (
+   <a href="https://www.google.net">
+     <h1>
+       Click me I am Gooooooooooogle
+     </h1>
+   </a>
+ );

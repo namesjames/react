b2681ec1c5150c48c915e33b589696e0cb576579
J ª/\/\ ∑ $ <james@galvanize.com>
2016-08-10 23:28:23 -0600

    Update app.js

--- app.js
+++ app.js
@@ -56,4 +56,45 @@ ReactDOM.render(
 	document.getElementById('app')
 );
 
+var myDiv = ( 
+<div className="big">I AM A BIG DIV NOW</div>);
 
+ReactDOM.render(myDiv, document.getElementById('app'));
+
+var profile = (
+  <div>
+    <h1>I AM JENKINS</h1>
+    <img src="images/jenkins.png" />
+    <article>
+      I LIKE TO SIT
+      <br />
+      JENKINS IS MY NAME
+      <br />
+      THANKS HA LOT
+    </article>
+  </div>
+);
+
+ReactDOM.render(
+  <h1>{2 + 3}</h1>,
+  document.getElementById('app')
+);
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var pi = (
+  <div>
+    <h1>
+      PI, YALL!
+    </h1>
+    <p>
+      {Math.PI.toFixed(20)}
+    </p>
+  </div>
+);
+
+ReactDOM.render(
+	pi,
+	document.getElementById('app')
+);

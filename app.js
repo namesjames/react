fead333d54f064f3e1a2bcc1bb95c3ca956a3af0
J ª/\/\ ∑ $ <james@galvanize.com>
2016-08-15 22:07:05 -0600

    Update app.js

--- app.js
+++ app.js
@@ -111,3 +111,23 @@ var theBestString = 'tralalalala i am da best';
 
 ReactDOM.render(<h1>{theBestString}</h1>, document.getElementById('app'));
 
+var gooseImg = <img src={goose} />;
+
+ReactDOM.render(
+  gooseImg,
+  document.getElementById('app')
+);
+
+function makeDoggy (e) {
+  e.target.setAttribute('src', 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-puppy.jpeg');
+  e.target.setAttribute('alt', 'doggy');
+}
+
+var kitty = (
+	<img 
+		src="https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-kitty.jpg" 
+		alt="kitty"
+		onClick={makeDoggy} />
+);
+
+ReactDOM.render(kitty, document.getElementById('app'));

478f272f503d5066c38d5f0587870bd9f7de5d55
J ª/\/\ ∑ $ <james@galvanize.com>
2016-08-16 22:32:47 -0600

    Update app.js

--- app.js
+++ app.js
@@ -131,3 +131,30 @@ var kitty = (
 );
 
 ReactDOM.render(kitty, document.getElementById('app'));
+
+ if (coinToss() == 'heads') {
+   var img = (
+     <img src={pics.kitty} />;
+   );
+} else {
+var img = (
+  <img src={pics.doggy} />;
+);
+}
+
+function coinToss () {
+  // Randomly return either 'heads' or 'tails'.
+  return Math.random() < 0.5 ? 'heads' : 'tails';
+}
+
+var pics = {
+  kitty: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-kitty.jpg',
+  doggy: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-puppy.jpeg'
+};
+
+var img = <img src={pics[coinToss() == 'heads' ? 'kitty' : 'doggy']} />;
+
+ReactDOM.render(
+	img, 
+	document.getElementById('app')
+);

f02c19e7b116f9fab3bb910ddd764a78b1779a87
J ª/\/\ ∑ $ <james@galvanize.com>
2016-08-18 23:58:02 -0600

    Update app.js

--- app.js
+++ app.js
@@ -179,3 +179,16 @@ var peopleLIs = people.map(function(person){
 });
 ReactDOM.render(<ul>{peopleLIs}</ul>,document.getElementById('app'));
 
+var people = ['Rowe', 'Prevost', 'Gare'];
+
+var peopleLIs = people.map(function(person, i){
+	 return <li key={'person_' + i}>{person}</li>;
+});
+
+ReactDOM.render(<ul>{peopleLIs}</ul>,document.getElementById('app'));
+
+var greatestDivEver = React.createElement(
+"div",
+null,
+"i am div"
+);

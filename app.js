9e6b479ce3fa9e9b3af70489ca6cb5a89ea38f52
J ª/\/\ ∑ $ <james@galvanize.com>
2016-08-23 22:11:59 -0600

    Update app.js

--- app.js
+++ app.js
@@ -192,3 +192,29 @@ var greatestDivEver = React.createElement(
 null,
 "i am div"
 );
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var QuoteMaker = React.createClass({
+  render: function () {
+    return (
+<blockquote>
+  <p>
+    What is important now is to recover our senses.
+  </p>
+  <cite>
+    <a target="_blank" 
+      href="http://bit.ly/1LvSLUA">
+      Susan Sontag
+    </a>
+  </cite>
+</blockquote>
+    );
+  }
+});
+
+ReactDOM.render(
+  <QuoteMaker />,
+  document.getElementById('app')
+);

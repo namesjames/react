f466bd755ec561e8a6718ac63a0682de72b0106d
J ª/\/\ ∑ $ <james@galvanize.com>
2016-08-24 22:03:57 -0600

    Update app.js

--- app.js
+++ app.js
@@ -218,3 +218,29 @@ ReactDOM.render(
   <QuoteMaker />,
   document.getElementById('app')
 );
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var owl = {
+  title: "Excellent Owl",
+  src: "https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-owl.jpg"
+};
+
+var Owl = React.createClass({
+render: function () {
+    return (
+      <div>
+	<h1>{owl.title}</h1>
+	<img 
+		src={owl.src} 
+    alt={owl.title} />
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <Owl />,
+  document.getElementById('app')
+);

1f380cd5c278ff0816be7a268498ab1058a7b24c
J ª/\/\ ∑ $ <james@galvanize.com>
2016-08-25 21:52:57 -0600

    Update app.js

--- app.js
+++ app.js
@@ -244,3 +244,30 @@ ReactDOM.render(
   <Owl />,
   document.getElementById('app')
 );
+
+var friends = [
+  {
+    title: "Yummmmmmm",
+    src: "https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-monkeyweirdo.jpg"
+  },
+  {
+    title: "Hey Guys!  Wait Up!",
+    src: "https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-earnestfrog.jpg"
+  },
+  {
+    title: "Yikes",
+    src: "https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-alpaca.jpg"
+  }
+];
+
+var Friend = React.createClass({
+render: function () {
+	var friend = friends[1];
+  return (
+    <div>
+			<h1>{friend.title}</h1>
+			<img src={friend.src} />    
+</div>
+  );
+}
+});

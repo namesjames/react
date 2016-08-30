0048213b5c81c183b0d8447b5a0838532c51d295
J ª/\/\ ∑ $ <james@galvanize.com>
2016-08-29 21:44:11 -0600

    Update app.js

--- app.js
+++ app.js
@@ -271,3 +271,36 @@ render: function () {
   );
 }
 });
+
+var friends = [
+  {
+    title: "Yummmmmmm",
+    src: "https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-monkeyweirdo.jpg"
+  },
+  {
+    title: "Hey Guys!  Wait Up!",
+    src: "https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-earnestfrog.jpg"
+  },
+  {
+    title: "Yikes",
+    src: "https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-alpaca.jpg"
+  }
+];
+
+var Friend = React.createClass({
+render: function () {
+	var friend = friends[2];
+  return (
+    <div>
+			<h1>{friend.title}</h1>
+			<img src={friend.src} />    
+</div>
+  );
+}
+});
+
+ReactDOM.render(
+  <Friend />,
+  document.getElementById('app')
+);
+

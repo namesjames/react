a28ea20466aea350e473511f9a13210b5676073d
J ª/\/\ ∑ $ <james@galvanize.com>
2016-09-08 22:41:44 -0600

    Update app.js

--- app.js
+++ app.js
@@ -340,3 +340,17 @@ name: 'James',
 
 ReactDOM.render(<MyName />, document.getElementById('app'));
 
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Button = React.createClass({
+  scream: function () {
+    alert('AAAAAAAAHHH!!!!!');
+  },
+
+  render: function () {
+    return <button onClick={this.scream}>AAAAAH!</button>;
+  }
+});
+ReactDOM.render(<Button />,
+document.getElementById('app'));

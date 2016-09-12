b1adda2fa43e2666d441ac230c1de086b3f15ce3
J ª/\/\ ∑ $ <james@galvanize.com>
2016-09-11 20:34:57 -0600

    Update app.js

--- app.js
+++ app.js
@@ -354,3 +354,20 @@ var Button = React.createClass({
 });
 ReactDOM.render(<Button />,
 document.getElementById('app'));
+
+var React = require('react');
+
+var NavBar = React.createClass({
+  render: function () {
+    var pages = ['home', 'blog', 'pics', 'bio', 'art', 'shop', 'about', 'contact'];
+    var navLinks = pages.map(function(page){
+      return (
+        <a href={'/' + page}>
+          {page}
+        </a>
+      );
+    });
+
+    return <nav>{navLinks}</nav>;
+  }
+});

9e4466b968945d92ea004febb628178e50a9bcfb
J ª/\/\ ∑ $ <james@galvanize.com>
2016-09-14 22:45:06 -0600

    Update app.js

--- app.js
+++ app.js
@@ -371,3 +371,20 @@ var NavBar = React.createClass({
     return <nav>{navLinks}</nav>;
   }
 });
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+var NavBar = require('./NavBar');
+
+var ProfilePage = React.createClass({
+  render: function () {
+    return (
+      <div>
+	<NavBar />
+        <h1>All About Me!</h1>
+        <p>I like movies and blah blah blah blah blah</p>
+        <img src="https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-monkeyselfie.jpg" />
+      </div>
+    );
+  }
+});

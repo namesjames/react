45fec466056d91fa06da9e022a05439e2d0fe995
J ª/\/\ ∑ $ <james@galvanize.com>
2016-10-03 22:20:07 -0600

    Update app.js

--- app.js
+++ app.js
@@ -703,3 +703,60 @@ var Example = React.createClass({
 });
 
 <Example />
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Mood = React.createClass({
+  getInitialState: function () {
+    return {
+      mood: 'good'    
+    };
+  },
+
+  toggleMood: function () {
+    var newMood = this.state.mood == 'good' ? 'bad' : 'good';
+    this.setState({ mood: newMood });
+  },
+
+  render: function () {
+    return (
+      <div>
+        <h1>I'm feeling {this.state.mood}!</h1>
+        <button onClick={this.toggleMood}>
+          Click Me
+        </button>
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(<Mood />, document.getElementById('app'));
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+
+var green = '#39D1B4';
+var yellow = '#FFD712';
+
+var Toggle = React.createClass({
+ getInitialState: 
+
+function () {
+    return {
+      color: green    
+    };
+  },
+  render: function () {
+    return (
+      <div style={{background: this.state.color}}>
+        <h1>
+          Change my color
+        </h1>
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(<Toggle />, document.getElementById('app'));

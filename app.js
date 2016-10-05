e9f16e3d0021d25a7efb4c65164a1caf9fb178aa
J ª/\/\ ∑ $ <james@galvanize.com>
2016-10-04 22:38:05 -0600

    Update app.js

--- app.js
+++ app.js
@@ -736,27 +736,34 @@ ReactDOM.render(<Mood />, document.getElementById('app'));
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-
 var green = '#39D1B4';
 var yellow = '#FFD712';
 
 var Toggle = React.createClass({
- getInitialState: 
-
-function () {
-    return {
-      color: green    
-    };
+  getInitialState: function () {
+    return { color: green };
   },
+  
+  changeColor: function () {
+    var color = this.state.color == green ? yellow : green;
+    this.setState({ color: color });
+  },
+  
   render: function () {
     return (
       <div style={{background: this.state.color}}>
         <h1>
           Change my color
         </h1>
+        <button onClick={this.changeColor}>
+          Change color
+        </button>
       </div>
     );
   }
 });
 
-ReactDOM.render(<Toggle />, document.getElementById('app'));
+ReactDOM.render(
+  <Toggle />,
+  document.getElementById('app')
+);

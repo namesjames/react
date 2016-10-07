65d79fce723a4631e2fb3dcaff24e30439c95127
J ª/\/\ ∑ $ <james@galvanize.com>
2016-10-06 21:09:09 -0600

    Update components.js

--- components.js
+++ components.js
@@ -11,3 +11,17 @@ ReactDOM.render(
   <MyComponentClass />,
   document.getElementById('app')
 );
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+
+var Parent = React.createClass({
+  getInitialState: function () {
+    return { name: 'Frarthur' };
+  },
+  
+  render: function () {
+    return <div></div>;
+  }
+});

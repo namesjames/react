faca3b9a6a6384046d4b39f2cdb8fd34223daa82
J ª/\/\ ∑ $ <james@galvanize.com>
2016-10-12 23:48:13 -0600

    Update components.js

--- components.js
+++ components.js
@@ -94,3 +94,31 @@ var ChildClass = React.createClass({
 });
 
 module.exports = ChildClass;
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Child = require('./Child');
+
+var Parent = React.createClass({
+  getInitialState: function () {
+    return { name: 'Frarthur' };
+  },
+  
+  changeName: function (newName) {
+    this.setState({
+      name: newName
+    });
+  },
+
+  render: function () {
+    return (
+    	<Child 
+    		name={this.state.name} />
+    );
+  }
+});
+
+ReactDOM.render(
+	<Parent />, 
+	document.getElementById('app')
+);

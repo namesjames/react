c9aa633f8895d7cb6c20804b2b4cdd2f1713ee22
J ª/\/\ ∑ $ <james@galvanize.com>
2016-10-18 23:30:26 -0600

    Update components.js

--- components.js
+++ components.js
@@ -179,3 +179,32 @@ ReactDOM.render(
 	<Parent />, 
 	document.getElementById('app')
 );
+
+var React = require('react');
+
+var Child = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <h1>
+          Hey my name is {this.props.name}!
+        </h1>
+        <select id="great-names">
+          <option value="Frarthur">
+            Frarthur
+          </option>
+
+          <option value="Gromulus">
+            Gromulus
+          </option>
+
+          <option value="Thinkpiece">
+            Thinkpiece
+          </option>
+        </select>
+      </div>
+    );
+  }
+});
+
+module.exports = Child;

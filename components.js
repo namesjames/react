adbefd26475383fdd30a31c54e712eb6537fc9df
J ª/\/\ ∑ $ <james@galvanize.com>
2016-10-21 23:02:48 -0600

    Update components.js

--- components.js
+++ components.js
@@ -183,3 +183,19 @@ ReactDOM.render(
 	document.getElementById('app')
 );
 
+var React = require('react');
+
+var Sibling = React.createClass({
+  render: function () {
+
+    return (
+      <div>
+        <h1>Hey, my name is Frarthur!</h1>
+        <h2>Don't you think Frarthur is the prettiest name ever?</h2>
+        <h2>Sure am glad that my parents picked Frarthur!</h2>
+      </div>
+    );
+  }
+});
+
+module.exports = Sibling;

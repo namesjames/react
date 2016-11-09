22f4037c01040add0e91196cca36d43044c19325
J ª/\/\ ∑ $ <james@galvanize.com>
2016-11-08 20:32:24 -0700

    Update components.js

--- components.js
+++ components.js
@@ -366,3 +366,19 @@ ReactDOM.render(
   <GuineaPigsContainer />, 
   document.getElementById('app')
 );
+
+var React = require('react');
+
+var GuineaPigs = React.createClass({
+  render: function () {
+    var src = this.props.src;
+    return (
+      <div>
+        <h1>Cute Guinea Pigs</h1>
+        <img src={src} />
+      </div>
+    );
+  }
+});
+
+module.exports = GuineaPigs;

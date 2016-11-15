207db4bef57033d82a28c36c68c9c3363356631a
J ª/\/\ ∑ $ <james@galvanize.com>
2016-11-14 21:15:30 -0700

    Update components.js

--- components.js
+++ components.js
@@ -382,3 +382,30 @@ var GuineaPigs = React.createClass({
 });
 
 module.exports = GuineaPigs;
+
+var MyComponentClass = React.createClass({
+  render: function(){
+    return <h1>Hello world</h1>;
+  }
+});
+
+function MyComponentClass () {
+  return <h1>Hello world</h1>;
+}
+
+ReactDOM.render(
+	<MyComponentClass />,
+	document.getElementById('app')
+);
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+function Friend () {
+  return <img src='https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-octopus.jpg' />;
+}
+
+ReactDOM.render(
+	<Friend />,
+	document.getElementById('app')
+);

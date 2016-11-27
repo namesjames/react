445539df737f2977f496c842a5f9a9d78bf32b22
J ª/\/\ ∑ $ <james@galvanize.com>
2016-11-26 20:02:55 -0700

    Update components.js

--- components.js
+++ components.js
@@ -441,3 +441,39 @@ function GuineaPigs (props) {
 }
 
 module.exports = GuineaPigs;
+
+var React = require('react');
+
+var BestSeller = React.createClass({
+  render: function () {
+    return (
+      <li>
+        Title: <span>
+          {this.props.title}
+        </span><br />
+        
+        Author: <span>
+          {this.props.author}
+        </span><br />
+        
+        Weeks: <span>
+          {this.props.weeksOnList}
+        </span>
+      </li>
+    );
+  }
+});
+
+module.exports = BestSeller;
+
+var React = require('react');
+
+var MessageDisplayer = React.createClass({
+  propTypes: {
+    message: React.PropTypes.string
+  },
+
+  render: function () {
+    return <h1>{this.props.message}</h1>;
+  }
+});

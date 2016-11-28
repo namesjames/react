48014d8fefdb2642f1171196be93909d4ade6d50
J ª/\/\ ∑ $ <james@galvanize.com>
2016-11-27 17:43:31 -0700

    Update components.js

--- components.js
+++ components.js
@@ -445,6 +445,11 @@ module.exports = GuineaPigs;
 var React = require('react');
 
 var BestSeller = React.createClass({
+	propTypes: {
+   	 title: React.PropTypes.string.isRequired,
+   	 author: React.PropTypes.string.isRequired,
+   	 weeksOnList: React.PropTypes.number.isRequired
+  },
   render: function () {
     return (
       <li>
@@ -477,3 +482,71 @@ var MessageDisplayer = React.createClass({
     return <h1>{this.props.message}</h1>;
   }
 });
+
+var React = require('react');
+
+var Runner = React.createClass({
+  propTypes: {
+    message:   React.PropTypes.string.isRequired,
+    style:     React.PropTypes.object.isRequired,
+    isMetric:  React.PropTypes.bool.isRequired,
+    miles:     React.PropTypes.number.isRequired,
+    milesToKM: React.PropTypes.func.isRequired,
+    races:     React.PropTypes.array.isRequired
+  },
+
+  render: function () {
+  	var miles = this.props.miles;
+    var km = this.props.milesToKM(miles);
+    var races = this.props.races.map(function(race, i){
+      return <li key={race + i}>{race}</li>;
+    });
+
+    return (
+      <div style={this.props.style}>
+        <h1>{this.props.message}</h1>
+        { this.props.isMetric && 
+          <h2>One Time I Ran {km} Kilometers!</h2> }
+        { !this.props.isMetric && 
+          <h2>One Time I Ran {miles} Miles!</h2> }
+        <h3>Races I've Run</h3>
+        <ul id="races">{races}</ul>
+      </div>
+    );
+  }
+});
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+var BestSeller = require('./BestSeller');
+
+var BookList = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <h1>Best Sellers</h1>
+        <div>
+          <ol>
+            <BestSeller 
+              title="Glory and War Stuff for Dads" 
+              author="Sir Eldrich Van Hoorsgaard" 
+              weeksOnList={10} />
+            <BestSeller 
+              title="The Crime Criminals!" 
+              author="Brenda Sqrentun" 
+              weeksOnList={2} />
+            <BestSeller
+              title="Subprime Lending For Punk Rockers" 
+              author="Malcolm McLaren" 
+              weeksOnList={600} />
+          </ol>
+        </div>
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <BookList />,
+  document.getElementById('app')
+);

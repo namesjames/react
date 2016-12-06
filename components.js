71eee6d37917fb1de3c4e8c033987ac3c9cff515
J ª/\/\ ∑ $ <james@galvanize.com>
2016-12-05 21:25:58 -0700

    Update components.js

--- components.js
+++ components.js
@@ -572,3 +572,70 @@ function MyComponentClass (props) {
 	var title = props.title;
   return <h1>{title}</h1>;
 }
+
+var React = require('react');
+
+var GuineaPigs = React.createClass({
+  render: function () {
+    var src = this.props.src;
+    return (
+      <div>
+        <h1>Cute Guinea Pigs</h1>
+        <img src={src} />
+      </div>
+    );
+  }
+});
+
+function GuineaPigs (props) {
+	var title = props.title;
+  return       <div>
+        <h1>Cute Guinea Pigs</h1>
+        <img src={src} />
+      </div>
+}
+
+module.exports = GuineaPigs;
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+var GuineaPigs = require('../components/GuineaPigs');
+
+var GUINEAPATHS = [
+  'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-guineapig-1.jpg',
+  'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-guineapig-2.jpg',
+  'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-guineapig-3.jpg',
+  'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-guineapig-4.jpg'
+];
+
+var GuineaPigsContainer = React.createClass({
+  getInitialState: function () {
+    return { currentGP: 0 };
+  },
+
+  nextGP: function () {
+    var current = this.state.currentGP;
+    var next = ++current % GUINEAPATHS.length;
+    this.setState({ currentGP: next });
+  },
+
+  interval: null,
+
+  componentDidMount: function () {
+    this.interval = setInterval(this.nextGP, 5000);
+  },
+
+  componentWillUnmount: function () {
+    clearInterval(this.interval);
+  },
+
+  render: function () {
+    var src = GUINEAPATHS[this.state.currentGP];
+    return <GuineaPigs src={src} />;
+  }
+});
+
+ReactDOM.render(
+  <GuineaPigsContainer />, 
+  document.getElementById('app')
+);

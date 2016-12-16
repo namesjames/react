db1dfefacf05b9cfa3a0fc336c9d73dcfc4d57df
J ª/\/\ ∑ $ <james@galvanize.com>
2016-12-15 22:19:54 -0700

    Update native.js

--- native.js
+++ native.js
@@ -195,3 +195,56 @@ const styles = StyleSheet.create({
     backgroundColor: 'skyblue',
   },
   boxLarge: {
+
+import React, { Component } from 'react'
+import { AppRegistry, ListView, View, Text, StyleSheet } from 'react-native'
+
+const rows = [
+  {id: 0, text: 'View'},
+  {id: 1, text: 'Text'},
+  {id: 2, text: 'Image'},
+  {id: 3, text: 'ScrollView'},
+  {id: 4, text: 'ListView'},
+]
+
+const rowHasChanged = (r1, r2) => r1.id !== r2.id
+
+const ds = new ListView.DataSource({rowHasChanged})
+
+class App extends Component {
+
+  state = {
+    dataSource: ds.cloneWithRows(rows)
+  }
+
+  renderRow = (rowData) => {
+    return (
+      <Text style={styles.row}>
+        {rowData.text}
+      </Text>
+    )
+  }
+
+  render() {
+    return (
+      <ListView
+        style={styles.container}
+        dataSource={this.state.dataSource}
+        renderRow={this.renderRow}
+      />
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  row: {
+    padding: 15,
+    marginBottom: 5,
+    backgroundColor: 'skyblue',
+  },
+})
+
+AppRegistry.registerComponent('App', () => App)

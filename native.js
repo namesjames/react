a69fbdaa026e346057ed2e763e0a447e684c9497
J ª/\/\ ∑ $ <james@galvanize.com>
2016-12-23 23:07:44 -0700

    Update native.js

--- native.js
+++ native.js
@@ -248,3 +248,60 @@ const styles = StyleSheet.create({
 })
 
 AppRegistry.registerComponent('App', () => App)
+
+
+import { AppRegistry, View } from 'react-native'
+
+import App from './App'
+
+AppRegistry.registerComponent('App', () => App)
+
+import React, { Component } from 'react'
+import { AppRegistry, View } from 'react-native'
+
+import List from './List'
+import Input from './Input'
+import Title from './Title'
+
+export default class App extends Component {
+
+  state = {
+    todos: ['Click to remove', 'Learn React Native', 'Write Code', 'Ship App'],
+  }
+
+  onAddTodo = (text) => {
+    const {todos} = this.state
+
+    this.setState({
+      todos: [text, ...todos],
+    })
+  }
+
+  onRemoveTodo = (index) => {
+    const {todos} = this.state
+
+    this.setState({
+      todos: todos.filter((todo, i) => i !== index),
+    })
+  }
+
+  render() {
+    const {todos} = this.state
+
+    return (
+      <View>
+        <Title>
+          To-Do List
+        </Title>
+        <Input
+          placeholder={'Type a todo, then hit enter!'}
+          onSubmitEditing={this.onAddTodo}
+        />
+        <List
+          list={todos}
+          onPressItem={this.onRemoveTodo}
+        />
+      </View>
+    )
+  }
+}
